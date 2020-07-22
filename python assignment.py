f18dd6494be49fcd4c2f7d19bea5c7d2fb5228c0
Nandhana18 <52792161+Nandhana18@users.noreply.github.com>
2020-07-22 17:05:19 +0530

    Add files via upload

---
+++ python assignment.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("GOOD MORNING")
+
+
+# In[3]:
+
+
+a="""hello
+letsupgrade"""
+print(a)
+
+
+# In[5]:
+
+
+print("goodbye")
+
+
+# In[6]:
+
+
+print("let\'s upgrade")
+
+
+# In[8]:
+
+
+print("welcome\tto world")
+
+
+# In[9]:
+
+
+print("good\n morning")
+
+
+# In[10]:
+
+
+a="mango"
+b="yellow"
+print(f'{a} is {b} in color')
+
+
+# In[12]:
+
+
+print("%s is %s in color"%(a,b))
+
+
+# In[13]:
+
+
+4**4
+
+
+# In[15]:
+
+
+10+80
+
+
+# In[16]:
+
+
+50-70
+
+
+# In[17]:
+
+
+90/3
+
+
+# In[18]:
+
+
+90//3
+
+
+# In[19]:
+
+
+50%4
+
+
+# In[20]:
+
+
+a=80
+b=80
+a==b
+
+
+# In[21]:
+
+
+c=50
+a==c
+
+
+# In[22]:
+
+
+c<=a
+
+
+# In[23]:
+
+
+b>=a
+
+
+# In[24]:
+
+
+a!=b
+
+
+# In[26]:
+
+
+str="good morning"
+"s" in str
+
+
+# In[27]:
+
+
+t=90
+w=90
+w is t
+
+
+# In[28]:
+
+
+id(t)
+
+
+# In[29]:
+
+
+id(w)
+
+
+# In[30]:
+
+
+id(-9)
+
